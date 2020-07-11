19637c0e65fad72b526672a012d613f569a4443f
CollinC <let.us.be@gmail.com>
2020-07-12 00:36:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,7 +26,12 @@
 #软件不定期同步大神库更新，适合一键下载到package目录下，用于openwrt编译
 #lede/package$下运行 或者openwrt/package$下运行
  #git clone https://github.com/kenzok8/litte.git
-
+ 
+ 
+#复制 SSH 连接命令粘贴到终端内执行，或者复制链接在浏览器中打开使用网页终端。（网页终端可能会遇到黑屏的情况，按 Ctrl + C 即可）
+#cd openwrt && make menuconfig
+#完成后按快捷键Ctrl+D或执行exit命令退出，后续编译工作将自动进行。
+#TIPS: 固件目录下有个config.seed文件，如果你需要再次编译可以使用它。
 
 git clone https://github.com/kenzok8/openwrt-packages.git
 
